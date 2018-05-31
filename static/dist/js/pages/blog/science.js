24e02a2e81c9b23abc392d278546a0e625c27697
mobius-crypt <mobiusndou@gmail.com>
2018-05-31 12:19:15 +0200

    news api implemented
    news api implemented

--- static/dist/js/pages/blog/science.js
+++ static/dist/js/pages/blog/science.js
@@ -1,3 +1,41 @@
 
 
-//TODO- load science documents
+
+// TODO- use google api for news and any other available API's to populate our blog upon launch,
+// TODO- blog pages must be summaries with links to the original article///
+
+// use this get request https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=3b2be7ef781441f4bde537854ffff2bf
+const apiKey = '3b2be7ef781441f4bde537854ffff2bf';
+
+const main = document.querySelector('main');
+
+//window.addEventListener('load', e =>{
+//updateNews();
+//}); https://newsapi.org/v2/everything?q=bitcoin&apiKey=3b2be7ef781441f4bde537854ffff2bf
+
+async function updateNews(){
+    const res = await fetch('https://newsapi.org/v2/everything?q=science&apiKey='+apiKey);
+    const json = await res.json();
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+
+}
+
+function createArticle(article){
+    return `
+<div class="box box-body with-border">
+    <div class="box box-header with-border">
+        <a href="${article.url}">
+            <h3 class="box-title">${article.title}</h3>
+        </a>   
+    </div>
+
+    <img src="${article.urlToImage}">
+    <blockquote>
+    ${article.description}
+    </blockquote>
+    
+</div>
+
+    `;
+}
+updateNews();
--- static/dist/js/pages/index.js
+++ static/dist/js/pages/index.js
@@ -125,3 +125,54 @@ document.getElementById('youtubeid').addEventListener("click", function () {
             }
         });
 });
+
+
+// use this get request https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=3b2be7ef781441f4bde537854ffff2bf
+const apiKey = '3b2be7ef781441f4bde537854ffff2bf';
+
+const advertCode = `
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-fb+5w+4e-db+86"
+     data-ad-client="ca-pub-7790567144101692"
+     data-ad-slot="7246754264"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+`
+
+const main = document.querySelector('main');
+
+//window.addEventListener('load', e =>{
+//updateNews();
+//}); https://newsapi.org/v2/everything?q=bitcoin&apiKey=3b2be7ef781441f4bde537854ffff2bf
+
+async function updateNews(){
+    const res = await fetch('https://newsapi.org/v2/everything?q="freelance" OR "cryptocurrency" OR "crypto Mining" OR "bitcoin" OR "Python27 Development" or "Python27 Developers" OR "Google Cloud" OR "AI" or "Artificial Intelligence"&pageSize=25&sortBy=publishedAt,relevancy, popularity&apiKey='+apiKey);
+    const json = await res.json();
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article){
+    return `
+<div class="box box-body with-border">
+    <div class="box box-header with-border">
+        <a href="${article.url}">
+            <h2 class="box-title">${article.title}</h2>
+        </a>   
+    </div>
+<div class="polaroid">
+    <img src="${article.urlToImage}" style="width:100%">
+    <div class="polatext">    
+     ${article.description}
+    </div>
+</div>
+
+</div>
+
+    `;
+}
+updateNews();
